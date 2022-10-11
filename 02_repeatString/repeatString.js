e220cdb4fd388af40065c86497622cec4a26e001
memoryIndex <r1ratheesh1@gmail.com>
2022-10-12 04:53:07 +0530

    complete 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (text,times) {
+    outputText = '';
 
+    if (times<0){
+        outputText='ERROR';
+    }
+    for (let i=0;i<times;i++){
+        outputText += text;
+    }
+    return outputText;
 };
 
 // Do not edit below this line
