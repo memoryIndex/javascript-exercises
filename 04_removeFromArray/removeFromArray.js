8b91f731452cfbd8b3f87e49c1714679b0be7806
memoryIndex <r1ratheesh1@gmail.com>
2022-10-15 04:53:26 +0530

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
 
+    newArray = [];
+
+    main: for(let i=0; i<arr.length; i++){
+        for (let j=0; j<toRemove.length;j++){
+            if (arr[i]===toRemove[j]){
+                continue main;
+            }
+        }
+        newArray.push(arr[i]);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
