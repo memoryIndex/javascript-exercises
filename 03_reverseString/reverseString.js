435740c2c0e2ec6b964fb48d9d04109670edc5c0
memoryIndex <r1ratheesh1@gmail.com>
2022-10-15 03:25:38 +0530

    complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    outputText = '';
+    endIndex = text.length;
+    for(let i=0;i<=text.length;i++){
+        currentLetter = text.charAt(endIndex);
+        outputText += currentLetter;
+        endIndex--;
+    }
+    return outputText;
 };
 
 // Do not edit below this line
