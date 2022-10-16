49bd380eeb4da1d0f709252d14eeed6e41cb250d
memoryIndex <r1ratheesh1@gmail.com>
2022-10-17 03:39:15 +0530

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    let sum = 0;
+    let i,j;
 
+    if (typeof(start) != 'number' || typeof(end) != 'number'){
+        return 'ERROR';
+    }
+
+    if (start<0 || end<0){
+        return 'ERROR';
+    }
+
+    if (start>end){
+        j = start;
+        i = end;
+    }else{
+        i = start;
+        j = end;
+    }
+    for(;i<=j;i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
