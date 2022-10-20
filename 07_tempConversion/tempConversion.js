92e79a59496d3dcbb65aafac4ac9e0a477af69e4
memoryIndex <r1ratheesh1@gmail.com>
2022-10-21 04:36:25 +0530

    complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    temp = temp-32;
+    temp = temp/1.8;
+    return Math.round(temp*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    temp = temp * 1.8;
+    temp = temp +32;
+    return Math.round(temp*10)/10;
 };
 
 // Do not edit below this line
