f146c3ee6068c77e93626c0b89875b7bd7b48e4f
memoryIndex <r1ratheesh1@gmail.com>
2022-10-21 04:53:21 +0530

    complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+};
+
+const subtract = function(num1, num2) {
+    let sum = num1 - num2;
+    return sum;
+};
+
+const sum = function(array) {
+    let sum = 0;
+    for (num of array){
+      sum += num;
+    }
+    return sum;
+};
+
+const multiply = function(array) {
+  let sum = 1;
+  for (num of array){
+    sum *= num;
+  }
+  return sum;
+};
+
+const power = function(num1, num2) {
+    return num1**num2;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(num) {
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+    let product = 1;
+    for(;num>0;num--){
+      product *= num;
+    }
+    return product;
 };
 
 // Do not edit below this line
