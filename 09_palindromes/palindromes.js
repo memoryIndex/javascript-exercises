82ca9e9e26e666592106fdfeee57f824b318efb4
memoryIndex <r1ratheesh1@gmail.com>
2022-10-22 05:13:30 +0530

    complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    text = text.replace(/ /g, "");
+    text = text.toLowerCase();
+    let end = text.length-1;
+
+    for (let start=0;start<end; start++){
+    let textstart = text[start];
+    let textend = text[end];
+
+        if (textstart === textend){
+            end--;
+            continue;
+        }else{
+            return false;
+        }
+    }
+
+    return true;
 
 };
 
