f82ca9f6914e88264f89dcc08f0c612f49f13432
memoryIndex <r1ratheesh1@gmail.com>
2022-10-23 04:36:23 +0530

    complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let tempname;
+    let age = 0;
+    const today = new Date()
 
+    for (let name of people){
+
+        if (name.yearOfDeath === undefined){
+            name.yearOfDeath = today.getFullYear();
+        }
+
+        let tempAge = name.yearOfDeath-name.yearOfBirth;
+        if(tempAge>age){
+            age = tempAge;
+            tempname = name;
+        }
+    }
+    return tempname;
 };
 
 // Do not edit below this line
