0ae558ee4b1dc973e089060a49bd530bf41bfefe
memoryIndex <r1ratheesh1@gmail.com>
2022-10-22 05:40:00 +0530

    complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(times) {
+    times = parseInt(times);
+    let fibPrevious = 1;
+    let fibNext = 1;
+    let fibsum = 0;
+
+    if (times<1){
+        return "OOPS";
+    }
+    if (times<3){
+        return 1;
+    }
+    for (let i=2;i<times;i++){
+
+        fibsum = fibPrevious+fibNext;
+        fibPrevious = fibNext;
+        fibNext = fibsum;
+    }
+    return fibsum;
 
 };
 
