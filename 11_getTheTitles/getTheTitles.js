36f20c0fd4b248403e81c25f893b4ab5faa475d3
memoryIndex <r1ratheesh1@gmail.com>
2022-10-23 03:39:31 +0530

    complete 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let array = [];
 
+    for (let items of books){
+        array.push(items.title);
+    }
+
+    return array;
 };
 
 // Do not edit below this line
